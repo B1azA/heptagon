e348f3d2c318b13b1edd6937de97ebd4604904c2
Josef Blažek <blaza004@gmail.com>
2022-08-23 13:45:24 +0200

    deleted sandbox

--- sandbox/src/main.rs
+++
@@ -1,8 +0,0 @@
-mod game;
-
-fn main() {
-    let main_loop = heptagon::main_loop::MainLoop::new("Heptagon");
-    let bundle = heptagon::rendering::bundle::Bundle::new(&main_loop.window);
-    let game = game::Game::new(&main_loop.window, bundle);
-    main_loop.run(game);
-}
